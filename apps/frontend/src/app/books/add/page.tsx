2560d85b1a7ac45057a97b9248cb897595ca68f7
DilZhan <chamithdilli@gmail.com>
2025-08-23 23:08:04 +0530

    [feat] create all pages and add sample heading

---
+++ apps/frontend/src/app/books/add/page.tsx
@@ -0,0 +1,7 @@
+export default function AddBook() {
+  return (
+    <div>
+      <h1>Add/Edit Book Page</h1>
+    </div>
+  );
+}
---
+++ apps/frontend/src/app/books/page.tsx
@@ -0,0 +1,7 @@
+export default function BookList() {
+  return (
+    <div>
+      <h1>Book List Page</h1>
+    </div>
+  );
+}
